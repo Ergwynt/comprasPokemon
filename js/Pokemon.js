9a82bfd8c9034aecb852269d4b6c7e5e8b319120
Carlos Aday <99324802+Ergwynt@users.noreply.github.com>
2026-09-06 12:19:38 +0100

    Update Pokemon.js

--- js/Pokemon.js
+++ js/Pokemon.js
@@ -1,11 +1,11 @@
 export default class Pokemon {
     
     constructor(data) {
-        this.name = data.name;                          // Nombre del pokemon
-        this.id = data.id;                              // Id del pokemon
-        this.pkm_front = data.sprites.front_default;    // Pokemon de frente
-        this.pkm_back = data.sprites.back_default;      // Pokemon de espaldas
-        this.pkm_type = data.types;               // Tipo del pokemon (Devuelve un array)
+        this.name = data.name; // Nombre del pokemon
+        this.id = data.id;  // Id del pokemon
+        this.pkm_front = data.sprites.front_default;// Pokemon de frente
+        this.pkm_back = data.sprites.back_default; // Pokemon de espaldas
+        this.pkm_type = data.types; // Tipo del pokemon (Devuelve un array)
         // Extraer el ataque y la defensa de las estadísticas
         const stats = data.stats;
         this.pkm_attack = stats.find(stat => stat.stat.name === "attack").base_stat;
